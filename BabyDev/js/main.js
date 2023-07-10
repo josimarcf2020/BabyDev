af0091038f515301971dcd012d3bd1efe0ccf635
Josimar da Cunha Ferreira <62252790+josimarcf2020@users.noreply.github.com>
2023-07-10 17:07:37 -0300

    Exibindo os dados do objeto no html

--- BabyDev/js/main.js
+++ BabyDev/js/main.js
@@ -1,18 +1,42 @@
 const url = "http://localhost:3000/api";
 
 function getAlunos() {
-    axios.get(url.concat("/alunos"))
+    axios.get(`${url}/alunos`)
         .then(
             response => {
-                const alunos = JSON.stringify(response.data);
-                listaDeAlunos.textContent = alunos;
-            })
+
+                const dados = response.data.result;
+                
+                let html = ''
+            
+                for(let i = 0; i < dados.length; i++){
+                    html += `<tr>
+                                <th scope="row">${dados[i].idAluno}</th>
+                                <td>${dados[i].nome}</td>
+                                <td>${dados[i].sobrenome}</td>
+                                <td>${dados[i].telefone}</td>
+                                <td>${dados[i].email}</td>
+                                <td><button class="btn btn-success" onclick="">Editar</button></td>
+                                <td><button class="btn btn-danger" onclick="deleteAluno(${dados[i].id})">Deletar</button></td>
+                            </tr>`;
+                }
+
+                document.getElementById('table-body').innerHTML = html;
+
+            }
+
+        )
         .catch(
-            error => console.log(error));
-}
-
-getAlunos();
-
-function listaAlunos() {
+            error => console.log(error)
+        )
     
+}
+    
+function deleteAluno(codigo) {
+    axios.delete(`${url}/alunos/${codigo}`)
+    .then(
+        alert("Deletado com sucesso!!!"),
+        getAlunos()
+    )
+    .catch(error => console.log(error))
 }
